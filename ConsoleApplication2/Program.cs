dea456b29600fef331404a9b5d16406dc20c1701
Opilane TTHK <opilane@tthk.ee>
2019-09-27 17:03:43 +0300

    first commit

---
+++ ConsoleApplication2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomGenerator = new Random();
+            int cpuRoll = randomGenerator.Next(1, 7);
+            Console.WriteLine("Viska täringut 1-6ni.");
+            int userRoll = int.Parse(Console.ReadLine());
+
+            if (userRoll > cpuRoll)
+            {
+                Console.WriteLine("Sa võitsid!");
+            
+            }
+
+            else if (userRoll < cpuRoll)
+            {
+                Console.WriteLine("sa kaotasid.");
+            }
+
+            else
+            {
+                Console.WriteLine("Mäng jäi viiki.");
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
